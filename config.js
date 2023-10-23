e2d67203c7c54fc44eae19c8e2d986ab43d11a41
Pranav Praveen <72447760+TwistedVision518@users.noreply.github.com>
2023-10-23 16:18:05 +0530

    Update config.js
    new background

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ const CONFIG = {
 
 	// General
 	name: 'Pranav',
-	imageBackground: false,
-	openInNewTab: true,
+	imageBackground: true,
+	openInNewTab: false,
 	twelveHourFormat: false,
 
 	// Greetings
